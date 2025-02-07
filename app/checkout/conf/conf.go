94a3e4616ffb233d79fece2cd29a46074116fe3b
CrisOrainday <1272556939wht@gmail.com>
2025-02-07 23:09:27 +0800

    change ip to docker internet

--- app/checkout/conf/conf.go
+++ app/checkout/conf/conf.go
@@ -36,6 +36,7 @@ type Config struct {
 	MySQL    MySQL    `yaml:"mysql"`
 	Redis    Redis    `yaml:"redis"`
 	Registry Registry `yaml:"registry"`
+	Nats	 Nats	  `yaml:"nats"`
 }
 
 type MySQL struct {
@@ -47,6 +48,10 @@ type Redis struct {
 	Username string `yaml:"username"`
 	Password string `yaml:"password"`
 	DB       int    `yaml:"db"`
+}
+
+type Nats struct {
+	Address string `yaml:"address"`
 }
 
 type Kitex struct {
